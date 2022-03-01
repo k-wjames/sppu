39c5d19184b644382f1cba9da83ac075cf1ddaac
w.kariukijames <kwjames9326@gmail.com>
2022-03-01 12:47:03 +0300

    Added a modal in the team section

--- js/script.js
+++ js/script.js
@@ -19,3 +19,27 @@ var swiper = new Swiper(".about-slider", {
     },
     loop:true,
   });
+
+  const open=document.getElementById('open');
+  const modal_container=document.getElementById('modal-container');
+  const close=document.getElementById('close');
+
+  open.addEventListener('click',()=>{
+    modal_container.classList.add('show');
+  });
+
+  close.addEventListener('click',()=>{
+    modal_container.classList.remove('show');
+  });
+
+  const open_pauline=document.getElementById('open-pauline');
+  const modal_container_pauline=document.getElementById('modal-container-pauline');
+  const close_pauline=document.getElementById('close_pauline');
+
+  open_pauline.addEventListener('click',()=>{
+    modal_container_pauline.classList.add('show');
+  });
+
+  close_pauline.addEventListener('click',()=>{
+    modal_container.classList.remove('show');
+  });
