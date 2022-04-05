5ca49f35b05a35aa2ede310bb89a1313e43ce560
Jeremy <jerywarne7@gmail.com>
2022-04-05 21:33:12 +0300

    added the doughnut chart file

---
+++ js/doughnut.js
@@ -0,0 +1,46 @@
+const chart = document.getElementById("doughnut").getContext("2d");
+
+const myCustomers = new Chart(chart, {
+  type: "doughnut",
+  data: {
+    labels: [
+      "National Gov",
+      "County Gov",
+      "Ministries",
+      "MDAs",
+      "Parastatals",
+      "NGOs",
+      "Other",
+    ],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12096, 9719, 3006, 5697, 7905, 9238, 6809],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
