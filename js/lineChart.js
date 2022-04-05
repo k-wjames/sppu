3550e64448a3a00b21e11c99393cf6183ea047c0
Jeremy <jerywarne7@gmail.com>
2022-04-05 21:32:49 +0300

    added the line chart

---
+++ js/lineChart.js
@@ -0,0 +1,30 @@
+const ctx = document.getElementById("lineChart").getContext("2d");
+const myChart = new Chart(ctx, {
+  type: "line",
+  data: {
+    labels: [
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+      "Jan",
+      "Feb",
+      "Mar",
+      "April",
+    ],
+    datasets: [
+      {
+        label: "",
+        data: [2028, 3500, 3750, 2855, 4002, 2900, 3228, 3500, 3750, 2855],
+        backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+        borderColor: ["rgba(255, 99, 132, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    resposive: true,
+  },
+});
