2d9504d1e2207eeb2fe51bdc1da0ad376617d76c
w.kariukijames <kwjames9326@gmail.com>
2022-04-20 12:34:17 +0300

    Uodated the linechart header title and color code

--- js/lineChart.js
+++ js/lineChart.js
@@ -5,7 +5,7 @@ const myChart = new Chart(ctx, {
     data: {
         labels: ['2012', '2013', '2014', '2015', '2016', '2017','2018', '2019', '2020', '2021'],
         datasets: [{
-            label: '',
+            label: 'Yearly Scores',
             data: [1.5, 3.2, 2.9, 2.5, 2.2,3.01, 3.4, 2.9, 2.3, 3.0],
             backgroundColor: [
                 'rgb(236,64,122)'
