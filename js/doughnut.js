57bf44bfafad33f4a7be8e7fe16e780b108727a8
w.kariukijames <kwjames9326@gmail.com>
2022-04-20 12:10:46 +0300

    Updated the stats section coloring code and data

--- js/doughnut.js
+++ js/doughnut.js
@@ -3,29 +3,25 @@ const chart = document.getElementById("doughnut").getContext("2d");
 const myCustomers = new Chart(chart, {
   type: "doughnut",
   data: {
-    labels: ["National Gov", "County Gov", "Ministries", "MDAs", "Parastatals", "NGOs", "Other"],
+    labels: ["National Gov", "County Gov", "Parastatals", "NGOs", "Other"],
     datasets: [
       {
         label: "# of Votes",
-        data: [12096, 9719, 3006, 5697, 7905, 9238, 6809],
+        data: [12096, 9719, 7905, 9238, 6809],
         backgroundColor: [
 
-          'rgb(68, 84, 106)',
-          'rgb(91, 155, 213)',
-          'rgb(237, 125, 49)',
-          'rgb(165, 165, 165)',
-          'rgb(68, 114, 196)',
-          'rgb(112, 173, 71)',
-          'rgb(112, 48, 160)'
+          'rgb(41,182,246)',
+          'rgb(236,64,122)',
+          'rgb(102,187,106)',
+          'rgb(255,167,38)',
+          'rgb(68, 114, 196)'
         ],
         borderColor: [
-          'rgb(68, 84, 106)',
-          'rgb(91, 155, 213)',
-          'rgb(237, 125, 49)',
-          'rgb(165, 165, 165)',
-          'rgb(68, 114, 196)',
-          'rgb(112, 173, 71)',
-          'rgb(112, 48, 160)'
+          'rgb(41,182,246)',
+          'rgb(236,64,122)',
+          'rgb(102,187,106)',
+          'rgb(255,167,38)',
+          'rgb(68, 114, 196)'
         ],
         borderWidth: 1,
       },
--- js/lineChart.js
+++ js/lineChart.js
@@ -3,16 +3,16 @@ const ctx = document.getElementById('lineChart').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec','Jan', 'Feb', 'Mar', 'April'],
+        labels: ['2012', '2013', '2014', '2015', '2016', '2017','2018', '2019', '2020', '2021'],
         datasets: [{
             label: '',
-            data: [2028, 3500, 3750, 2855, 4002, 2900,3228, 3500, 3750, 2855],
+            data: [1.5, 3.2, 2.9, 2.5, 2.2,3.01, 3.4, 2.9, 2.3, 3.0],
             backgroundColor: [
-                'rgb(68, 84, 106)'
+                'rgb(236,64,122)'
                 
             ],
             borderColor: [
-                'rgb(68, 84, 106)'
+                'rgb(236,64,122)'
                 
             ],
             borderWidth: 1
