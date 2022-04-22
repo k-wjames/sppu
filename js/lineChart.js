2b4ad4aa9891986dd565bfebb18a02b3958f5509
w.kariukijames <kwjames9326@gmail.com>
2022-04-22 10:28:51 +0300

    Updated linechart data

--- js/lineChart.js
+++ js/lineChart.js
@@ -3,16 +3,17 @@ const ctx = document.getElementById('lineChart').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['2012', '2013', '2014', '2015', '2016', '2017','2018', '2019', '2020', '2021'],
+        labels: ['2018', '2019', '2020', '2021'],
         datasets: [{
             label: 'Yearly Scores',
-            data: [1.5, 3.2, 2.9, 2.5, 2.2,3.01, 3.4, 2.9, 2.3, 3.0],
+            data: [ 3.2150, 3.2540,  2.98360,  3.1100],
             backgroundColor: [
-                'rgb(236,64,122)'
+                
+                'rgb(41,182,246)'
                 
             ],
             borderColor: [
-                'rgb(236,64,122)'
+                'rgb(41,182,246)'
                 
             ],
             borderWidth: 1
