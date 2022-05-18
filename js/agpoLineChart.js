e794be5026b3b2f8366c4c377e226dbc555aa79a
w.kariukijames <kwjames9326@gmail.com>
2022-05-18 19:22:34 +0300

    Added shared documents

--- js/agpoLineChart.js
+++ js/agpoLineChart.js
@@ -1,17 +1,17 @@
-var xValues = ['2017/2018','2018/2019', '2019/2020', '2020/2021'];
-var yValues = [ 329638058.49,346864481.86,190725350.94,141559349.35];
+var xValues = ['2017/2018', '2018/2019', '2019/2020', '2020/2021'];
+var yValues = [329638058.49, 346864481.86, 190725350.94, 141559349.35];
 
 new Chart("agpoLineChart", {
   type: "line",
   data: {
     labels: xValues,
     datasets: [{
-        label:'Amount in Kshs.',
+      label: 'Amount in Kshs.',
       backgroundColor: 'rgb(41,182,246)',
       borderColor: 'rgb(41,182,246)',
       borderWidth: 1,
       data: yValues
     }]
   },
-  options:{resposive:true}
+  options: { resposive: true }
 });
