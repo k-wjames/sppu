bfbdbea299502ec0fd8122dd30c1a68bdcfba845
w.kariukijames <kwjames9326@gmail.com>
2022-05-18 13:07:21 +0300

    Updated reports

--- js/agpoLineChart.js
+++ js/agpoLineChart.js
@@ -1,31 +1,3 @@
-
-// const ctx = document.getElementById('agpoLineChart').getContext('2d');
-// const myChart = new Chart(ctx, {
-//     type: 'line',
-//     data: {
-//         labels: ['2017/2018','2018/2019', '2019/2020', '2020/2021'],
-//         datasets: [{
-//             label: 'Yearly Scores',
-//             data: [   329638058.49,346864481.86,190725350.94,141559349.35 ],
-//             backgroundColor: [
-                
-//                 'rgb(41,182,246)'
-                
-//             ],
-//             borderColor: [
-//                 'rgb(41,182,246)'
-                
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         resposive:true
-//     }
-// });
-
-
-
 var xValues = ['2017/2018','2018/2019', '2019/2020', '2020/2021'];
 var yValues = [ 329638058.49,346864481.86,190725350.94,141559349.35];
 
