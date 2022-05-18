025485db6027179438970207e3e4face90f8d7fd
w.kariukijames <kwjames9326@gmail.com>
2022-05-18 19:26:15 +0300

    Added shared documents

--- js/participantsLineChart.js
+++ js/participantsLineChart.js
@@ -1,8 +1,9 @@
 var xValues = ['2015/2016','2016/2017','2017/2018','2018/2019', '2019/2020', '2020/2021'];
 var yValues = [18765,18349,18738,22668, 13234,17543];
+var barColors = ["red", "green", "blue", "orange", "brown"];
 
 new Chart("participantsLineChart", {
-  type: "line",
+  type: "bar",
   data: {
     labels: xValues,
     datasets: [{
