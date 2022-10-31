ff63017c24bccdd4fc2414cf0d6277975efc5bdc
w.kariukijames <kwjames9326@gmail.com>
2022-10-31 14:57:41 +0300

    Updates

--- js/bkbkLineChart.js
+++ js/bkbkLineChart.js
@@ -1,5 +1,5 @@
-var xValues = ['2017/2018', '2018/2019', '2019/2020', '2020/2021'];
-var yValues = [388972830.43, 306037355.93, 489239055.22, 359575781.33];
+var xValues = ['2017/2018', '2018/2019', '2019/2020', '2020/2021', '2021/2022'];
+var yValues = [388972830.43, 306037355.93, 489239055.22, 359575781.33, 281170351.35];
 var barColors = ["red", "green", "blue", "orange", "brown"];
 
 new Chart("bkbkLineChart", {
@@ -8,7 +8,8 @@ new Chart("bkbkLineChart", {
         labels: xValues,
         datasets: [{
            label:'Amount in Kshs.',
-            backgroundColor: barColors,
+           backgroundColor: 'rgb(41,182,246)',
+           borderColor: 'rgb(41,182,246)',
             data: yValues
         }]
     },
