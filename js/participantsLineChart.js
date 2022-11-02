924a86de826f2384324f21d18892811329103dc5
w.kariukijames <kwjames9326@gmail.com>
2022-11-02 14:38:06 +0300

    Updates

--- js/participantsLineChart.js
+++ js/participantsLineChart.js
@@ -1,5 +1,5 @@
 var xValues = ['2015/16','2016/17','2017/18','2018/19', '2019/20', '2020/21', '2021/22'];
-var yValues = [18765,18349,18738,22668, 13234,17543];
+var yValues = [18765,18349,18738,22668, 13234,17543, 19098 ];
 var barColors = ["red", "green", "blue", "orange", "brown"];
 
 new Chart("participantsLineChart", {
